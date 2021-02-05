aa7b41389172bc9e9f9d1f9c684e1b4812c39842
Francisco Barrientos <francisco.barrientos@usach.cl>
2021-02-05 01:07:46 -0300

    Agregar carpetas components y hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { CounterApp } from './components/01-useState/CounterApp';
+//import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <HookApp />,
+    <SimpleForm />,
   document.getElementById('root')
 );
